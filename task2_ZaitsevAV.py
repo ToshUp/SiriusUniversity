7593f0c5ce59729476f57890d144ff9f207e7d2a
ToshUp <zaitsev_anton@mail.ru>
2022-10-17 11:10:29 +0300

    Add files

--- task2_ZaitsevAV.py
+++ task2_ZaitsevAV.py
@@ -1,123 +1,48 @@
-from math import atan, acos, sqrt, sin, cos, pi, atan2
-from sys import argv
-#
-#
-#
-def rocket_attack(file_name, radius):
-    # file_name = argv[1]
-    try:
-        # radius = float(argv[2])
-        if radius < 1:
-            raise Exception
-    except:
-        print("Radius is wrong")
-        quit()
-    # file_name = "input.txt"
-    # radius = 100
-    x = []
-    y = []
-    v = []
+import numpy as np
+import random
+import sys
 
-    # file read
-    try:
-        with open(file_name) as f:
-            lines = list(filter(None, f.read().split('\n')))
-    except:
-        print("File reading error")
-        quit()
+def hit(map, x_0, y_0, R):
+    damage = 0
+    for y in range(max(y_0 - R, 0), min(y_0 + R + 1, 100)):
+        for x in range(max(x_0 - R, 0), min(x_0 + R + 1, 100)):
+            if (x - x_0)**2 + (y - y_0)**2 <= R**2:
+                damage += map[y, x]
+    return damage
 
-    # file parse
-    i = 0
-    for line in lines:
-        xi, yi, vi = [int(elem) for elem in line.strip().split(' ')]
-        x.append(xi)
-        y.append(yi)
-        v.append(vi)
-        i = i + 1
+# #генерим данные
+# n=100
+# f = open('trgts.txt','w')
+# for i in range(0,n):
+#     for j in range(0,n):
+#         if random.randint(0,100)==1:
+#             f.write (str(i)+' '+str(j)+' '+ str(random.randint(1,100))+'\n')
 
-    n = 101
-    m = 101
-    field = [[0] * n for i in range(m)]
+# f.close()
 
-    for i in range(0, len(x)):
-        field[x[i]][y[i]] += v[i]
+name = sys.argv[1]
+R = int(sys.argv[2])
+#R = 10
+#name = 'trgts.txt'
+map = np.zeros((100, 100))
 
-    x = []
-    y = []
-    v = []
 
-    for i in range(0, n):
-        for j in range(0, m):
-            if field[i][j] != 0:
-                x.append(i)
-                y.append(j)
-                v.append(field[i][j])
+pts = np.loadtxt(name, int)
 
-    v_max_global = 0
-    x_strike = 0
-    y_strike = 0
 
-    # go for every point
-    for i in range(0, len(x)):
+for x, y, w in pts:
+    map[y, x] += w
 
-        # dictionary of angles
-        angles = []
-        types = []
-        vv = []
+coord_x = -1
+coord_y = -1
+damage_max = 0
 
-        v_current = 0
+for y in range(100):
+    for x in range(100):
+        damage = hit(map, x, y, R)
+        if damage > damage_max:
+            damage_max = damage
+            coord_x = x
+            coord_y = y
 
-        # check any other point
-        for j in range(0, len(x)):
-
-            # distance between point i and j
-            distance = sqrt((x[i] - x[j]) ** 2 + (y[i] - y[j]) ** 2)
-
-            # distance between point O(xi - r, yi) and j
-            # distance_including = sqrt((x[i] - radius - x[j]) ** 2 + (y[i] - y[j]) ** 2)
-
-            # condition of inclusion
-            if distance <= radius * 2:
-                # computing the params A B
-                A = atan2(y[i] - y[j], x[i] - x[j])
-                B = acos(distance / 2 / radius)
-
-                # computing the angles
-                alpha = A - B
-                beta = A + B
-
-                # adding angles in the dict
-                angles.append(alpha)
-                types.append(1)
-                vv.append(v[j])
-
-                angles.append(beta)
-                types.append(0)
-                vv.append(v[j])
-
-        # sorting the dict
-        angles_type_vv = [list(x) for x in zip(angles, types, vv)]
-
-        sorted_angels = sorted(angles_type_vv)
-
-        # finding max v value
-        v_max_local = v_current
-        x_max = 0
-        y_max = 0
-        for element in sorted_angels:
-            if element[1] == 1:
-                v_current = v_current + element[2]
-                if v_current > v_max_local:
-                    v_max_local = v_current
-                    x_max = x[i] - radius * cos(element[0])
-                    y_max = y[i] - radius * sin(element[0])
-            else:
-                v_current = v_current - element[2]
-        if v_max_local > v_max_global:
-            v_max_global = v_max_local
-            x_strike = x_max
-            y_strike = y_max
-
-    # print(summa)
-    # print("max v =", v_max_global)
-    print((x_strike, y_strike, v_max_global))
+print(damage_max, coord_x, coord_y)
